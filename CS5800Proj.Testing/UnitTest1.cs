733af7779cf239ae89d4b5c8557cc99769fd7cff
NoahDouglas7344 <noah-douglas@uiowa.edu>
2021-02-23 20:21:44 -0600

    Automated Testing
    Added XUnit Testing

---
+++ CS5800Proj.Testing/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace CS5800Proj.Testing
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void OneEqualsOne()
+        {
+            Assert.True(1==1);
+        }
+
+        [Fact]
+        public void OneIsNotTwo()
+        {
+            Assert.False(1 == 2);
+        }
+    }
+}
