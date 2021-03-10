3b7c0dbd7cdda3bc5c1fd71d959e391711e1ccef
NoahDouglas7344 <noah-douglas@uiowa.edu>
2021-03-09 22:06:57 -0600

    Implemented Account Creation and Added a few of the Unit Tests

--- CS5800Proj.Testing/UnitTest1.cs
+++ CS5800Proj.Testing/UnitTest1.cs
@@ -5,37 +5,71 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Xunit;
 
 
-//comment Austin Wittenburg 
 
 namespace CS5800Proj.Testing
 {
-    public class UnitTest1
+    // Noah Douglas Unit Tests
+    public class LoginTests
     {
-
         [Theory]
         [InlineData("Noah", "password2")]
-        public void LogOnBadCredentials(string value1, string value2)
+        public void BadCredentials(string user, string passWord)
         {
+
             var pageModel = new CS5800Proj.Pages.LogInModel();
             var httpContext = new DefaultHttpContext();
             
             //act                
-            var result = pageModel.OnPost(value1,value2);
+            var result = pageModel.OnPost(user,passWord);
 
             Assert.IsType<PageResult>(result);
         }
 
         [Theory]
         [InlineData("Noah","password1")]
-        public void LogOnGoodCredentials(string value1, string value2)
+        public void GoodCredentials(string user, string passWord)
         {
+            //arrange
             var pageModel = new CS5800Proj.Pages.LogInModel();
             var httpContext = new DefaultHttpContext();
 
             //act                
-            var result = pageModel.OnPost(value1, value2);
+            var result = pageModel.OnPost(user, passWord);
 
+            //assert
             Assert.IsType<RedirectToPageResult>(result);
+        }
+    }
+
+    public class AccountCreationTests
+    {
+        [Theory]
+        [InlineData("testNewUser", "password1", "password2","Donor")]
+        public void PassWordsMismatch(string user, string passWord1, string passWord2, string userType)
+        {
+            //arrange
+            var pageModel = new CS5800Proj.Pages.AccountCreationModel();
+            var httpContext = new DefaultHttpContext();
+
+            //act                
+            var result = pageModel.OnPost(user, passWord1, passWord2, userType);
+
+            //assert
+            Assert.IsType<PageResult>(result);
+        }
+        [Theory]
+        [InlineData("testNewUser", "password1", "password1", "Select...")]
+        public void DidNotSelectAccountType(string user, string passWord1, string passWord2, string userType)
+        {
+            //arrange
+            var pageModel = new CS5800Proj.Pages.AccountCreationModel();
+            var httpContext = new DefaultHttpContext();
+
+            //act                
+            var result = pageModel.OnPost(user, passWord1, passWord2, userType);
+
+            //assert
+            Assert.IsType<PageResult>(result);
         }
     }
 }
---
+++ CS5800Proj/Pages/AccountCreation.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
+namespace CS5800Proj.Pages
+{
+    public class AccountCreationModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string userName { get; set; }
+        public string passWord { get; set; }
+        public string passWord2 { get; set; }
+        public string userType { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost(string userName, string passWord,string passWord2, string userType)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+            if(passWord == passWord2 && passWord.Length>2 && userName.Length >2 && userType != "Select...")
+            {
+                using var connection = new MySqlConnection("server=localhost;port=3306;database=testDB;user=root;password=CS5800Team5");
+                {
+                    connection.Open();
+
+                    using var command = new MySqlCommand("INSERT INTO testUser(userName, passWord, userType) values('"+userName+"', '"+passWord+"', '"+userType+"')", connection);
+                    var adapter = command.ExecuteNonQuery();
+                    if(adapter > 0)
+                        return RedirectToPage("./Home");
+                }
+            }
+            return Page();
+        }
+    }
+}
