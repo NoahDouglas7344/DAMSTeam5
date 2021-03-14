56636a05c714d1e49cfeb894df117b52bddf3e38
Joe Chapski <joseph-chapski@uiowa.edu>
2021-03-13 19:44:46 -0600

    Added admin delete functionality

--- CS5800Proj/Pages/Delete.cshtml.cs
+++ CS5800Proj/Pages/Delete.cshtml.cs
@@ -1,28 +1,64 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace CS5800Proj.Pages
 {
     public class DeleteModel : PageModel
     {
-        [BindProperty(SupportsGet = true)]
-        public string donation_item { get; set; }
+
+        public List<String> names = new List<String>();
+        [Display(Name = "Items:")]
+
+        public int del_id { get; set; }
+        //public string del_items { get; set; }
+        
+
+        
 
         public void OnGet()
         {
+            // List<String> names = new List<String>();
+            using var connection = new MySqlConnection("server=localhost;port=3306;database=testDB;user=root;password=CS5800Team5");
+            {
+                connection.Open();
+
+                using var command = new MySqlCommand("SELECT name FROM donationitems", connection);
+                {
+                    using (MySqlDataReader sdr = command.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            names.Add(sdr["name"].ToString());
+                        }
+                    }
+                }
+            }
         }
 
         public IActionResult OnPost()
         {
+            string id = Request.Form["del_id"];
             if (ModelState.IsValid == false)
             {
                 return Page();
             }
-            return RedirectToPage("./Home");
+            using var connection = new MySqlConnection("server=localhost;port=3306;database=testDB;user=root;password=CS5800Team5");
+            {
+                connection.Open();
+
+                using var command = new MySqlCommand("DELETE FROM donationitems where name = '" + id + "'", connection);
+                var adapter = command.ExecuteNonQuery();
+                if (adapter > 0)
+                    return RedirectToPage("./Home");
+            }
+
+            return Page();
         }
     }
 }
