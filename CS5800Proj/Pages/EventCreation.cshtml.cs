a150fbbefccf07a6f5d5123f2d6f679f770a086e
austinwittenburg <awittenburg@uiowa.edu>
2021-03-14 16:44:10 -0500

    Added database functionality to EventCreation

--- CS5800Proj/Pages/EventCreation.cshtml.cs
+++ CS5800Proj/Pages/EventCreation.cshtml.cs
@@ -14,12 +14,27 @@ namespace CS5800Proj.Pages
 		{
 		}
 
-		public void OnPost()
+		public IActionResult OnPost()
 		{
 			var recipient = Request.Form["recipient"];
 			var time = Request.Form["time"];
 			var location = Request.Form["location"];
-			
+
+			if (recipient == "" || time == "" || location == "")
+			{
+				return Page();
+			}
+
+			using var connection = new MySqlConnection("server=localhost;port=3306;database=testDB;user=root;password=CS5800Team5");
+			{
+				connection.Open();
+
+				using var command = new MySqlCommand("INSERT INTO testEvents(recipient, time, location) values('"+recipient+"', '"+time+"', '"+location+"')", connection);
+				var adapter = command.ExecuteNonQuery();
+				if (adapter > 0)
+					return RedirectToPage("./Home");
+			}
+			return Page();
 		}
 	}
 }
