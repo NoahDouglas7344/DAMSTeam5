3e6f31319d65aaf366a888565a6b5fd509a9599c
austinwittenburg <awittenburg@uiowa.edu>
2021-03-15 15:14:56 -0500

    Added a confirmation page to let the user know the creation was sucessful.

---
+++ CS5800Proj/Pages/EventConfirmation.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS5800Proj.Pages
+{
+	public class EventConfirmationModel : PageModel
+	{
+		public void OnGet()
+		{
+		}
+
+		public IActionResult OnPost()
+		{
+			return RedirectToPage("./Home");
+		}
+	}
+}
