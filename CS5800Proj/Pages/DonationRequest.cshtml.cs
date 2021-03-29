7dd75aba41ae330e0aa9e5edada134391e9e80e4
NoahDouglas7344 <noah-douglas@uiowa.edu>
2021-03-29 14:12:33 -0500

    Donation Request Working

--- CS5800Proj/Pages/DonationRequest.cshtml.cs
+++ CS5800Proj/Pages/DonationRequest.cshtml.cs
@@ -4,26 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace CS5800Proj.Pages
 {
     public class DonationRequestModel : PageModel
     {
         [BindProperty(SupportsGet = true)]
-        public string disaster { get; set; }
-        
+        public string location { get; set; }
+        public string date { get; set; }
+        public string type { get; set; }
+        public string amount { get; set; }
 
         public void OnGet()
         {
 
         }
-        public IActionResult OnPost()
+        public IActionResult OnPost(string location, string date, string type, string amount)
         {
             if (ModelState.IsValid == false)
             {
                 return Page();
             }
-            return RedirectToPage("./Home");
+            if (location.Length > 2)
+            {
+                using var connection = new MySqlConnection("server=localhost;port=3306;database=testDB;user=root;password=CS5800Team5");
+                {
+                    connection.Open();
+
+                    using var command = new MySqlCommand("INSERT INTO Requests(DisasterLocation, DisasterDate, Recipient, RequestType, RequestQuantity)" +
+                        " VALUES('" + location + "','" + date + "','Noah', '" + type + "', '" + amount + "')", connection);
+                    var adapter = command.ExecuteNonQuery();
+                    if (adapter > 0)
+                        return RedirectToPage("./Home");
+                }
+            }
+            return Page();
         }
     }
 }
